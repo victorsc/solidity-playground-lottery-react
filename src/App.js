8e5a989ee71dc5d6126a7fa8eb4bd84607f5bc75
victorsc <victorscaiceanu@gmail.com>
2022-02-24 00:42:06 +0100

    enter lottery and pick winner

--- src/App.js
+++ src/App.js
@@ -1,27 +1,80 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import web3 from './web3';
 import lottery from './lottery';
+import web3 from './web3';
 
 class App extends React.Component {
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: '',
+    message: ''
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {manager: ''};
-  }
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager, players, balance });
+  }
 
-    this.setState({ manager });
+  onSubmit = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'waiting on transaction success...' });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: `You have entered the lottery with ${this.state.value}` });
+  }
+
+  onClick = async event => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: 'pick winner action initiated...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    this.setState({ message: `A winner has been picked!` });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This constract is managed by {this.state.manager}</p>
+        <p>
+          This constract is managed by {this.state.manager}.<br />
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input 
+              onChange={event => this.setState({value: event.target.value})}
+              value={this.state.value}
+            />
+          </div>
+          <button>Enter</button>
+          <hr />
+
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={this.onClick}>Pick winner</button>
+
+        </form>
+        <hr />
+
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
